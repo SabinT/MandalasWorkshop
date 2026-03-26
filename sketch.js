0955990372a642ce609078a097e909f56225aa37
sabin <hello@sabin.art>
2026-03-26 14:40:34 -0700

    simplify

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-// --------- SYSTEM CODE: Don't change unless you know what you're doing! ---------
+// --------- SYSTEM CODE: Don't change this! ----------
 const UI = new WorkshopSketchUI();
 const CANVAS_SIZE = 1000;
 const MOTIF_SIZE = 10;
@@ -8,7 +8,7 @@ function setup() {
     setMotifSize(MOTIF_SIZE);
     angleMode(DEGREES);
 }
-// -------------------- END OF SYSTEM CODE ----------------------------------------
+// --------- END OF SYSTEM CODE -----------------------
 
 // Change background color here! It has to be a hex code
 const MANDALA_BG = "#accbbc";
@@ -20,7 +20,6 @@ function draw() {
     drawPolarGrid(UI.getPolarGridDivisions(), 0, width / 2);
 
     // ------------- YOUR MANDALA DESIGN HERE! ------------------
-    // **********************************************************
     // Set stroke and fill colors
     stroke("black");
     fill("white");
@@ -28,7 +27,7 @@ function draw() {
     // Syntax: ring({ shape: motifFunction, n: [reptition count], r1: [start radius], r2: [end radius] });
     ring({ shape: yourMotif, n: 24, r1: 250, r2: 300 });
 
-    // Try 'offset' and 'gapDegrees' parameters to stagger / space out motifs within a ring!
+    // Try optional 'offset' and 'gapDegrees' parameters to stagger / space out motifs within a ring!
     // ring({ shape: yourMotif, n: 24, r1: 300, r2: 320, offset: 0.5, gapDegrees: 10 });
 
     // ------------- END OF YOUR DESIGN ------------------
@@ -40,24 +39,17 @@ function draw() {
     }
 }
 
-// A motif is a simple function that used commands like mLine(), mTriangle(), mBox(), etc. to draw a design within a 20x20 box centered at (0, 0).
+// A motif is a simple function that used commands like mLine(), mTriangle(), etc.
+// The design space is a 20x20 box centered at (0,0)
 function yourMotif() {
-    
     mTriangle(-10, 10, 0, -10, 10, 10);
-    
-    // Try these other drawing commands as well!
-
-    // mLine(-10, -10, 10, 10);
-
-    // mCircle(0, 0, 10);
-
-    // mQuad(-10, -10, -5, 10, 5, 10, 10, -10);    
-
-    // mDot(0, 0, 10);
-
-    // mEllipse(0, 0, 5, 10);
-    
-    // mBezier(-10, -10, -5, 5, 10, -10, 10, 10);
 }
 
 // Add new motif functions here! You can create as many as you like.
+// Try these other drawing commands inside your motif functions!
+// mLine(-10, -10, 10, 10);
+// mCircle(0, 0, 10);
+// mQuad(-10, -10, -5, 10, 5, 10, 10, -10);    
+// mDot(0, 0, 10);
+// mEllipse(0, 0, 5, 10);
+// mBezier(-10, -10, -5, 5, 10, -10, 10, 10);
